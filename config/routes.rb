efb40273a1f352df6ed995b1ba0196734e57fad5
NJXZhang <ninasax@outlook.com>
2016-12-01 15:29:13 -0500

    Change the path of search page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :search_tests
+  resources :search_tests, :path => "search_1"
   resources :search_tests
   resources :search_tests
   resources :search_tests
